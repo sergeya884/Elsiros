0079517688e8fb59e5f925c676d6360d6403c7b7
sergeya884 <agafonov.sa@phystech.edu>
2024-08-01 23:53:04 +0300

    add texture trek to sprint

--- controllers/sprint/sprint.py
+++ controllers/sprint/sprint.py
@@ -4,16 +4,9 @@ import subprocess
 from pathlib import Path
 import json
 from controller import Supervisor, AnsiCodes, Node
-import random
-import time
-# import sys
-# sys.path.append('C:\Elsiros\controllers\Robofest_TEAM\Soccer\Motion')
-# from class_Motion import Motion1
-# Path1 = os.chdir('../Soccer/Motion')
-# print(sys.path)
-# print(Path.cwd())
-# from class_Motion import Motion1 
-is_pen = True
+
+is_pen = False
+center_of_rows = (0.0, 1.05, 2.1)
 
 # C:\Elsiros\controllers\Robofest_TEAM\Soccer\Motion\class_Motion.py
 
@@ -23,9 +16,6 @@ is_pen = True
 supervisor = Supervisor()
 time_step = int(supervisor.getBasicTimeStep())
 if is_pen:
-    # pen2 = supervisor.getDevice('pen')
-    # pen1.write(True)
-    # pen_translation = pen1.getField('translation')
     pen1 = supervisor.getFromDef('PEN_1')
 
     pen1_pen = supervisor.getDevice('pen')
@@ -33,22 +23,12 @@ if is_pen:
     pen1_leadSize = pen1.getField('leadSize')
     pen1_pen.write(True)
     pen1_translation.setSFVec3f([1.0, 0.0, 0.001])
-# texture_scale = supervisor.getFromDef('CUST_TEXTURE1').getField('scale')
-# texture_point = supervisor.getFromDef('CUST_TEXTURE1').getField('pointCoord')
 
-# [
-#               0 -0.5 0.01
-#               3 -0.5 0.01
-#               3 0.5 0.01
-#               0 0.5 0.01
-#             ]
+texture_points = []
+texture_points.append(supervisor.getFromDef('CUST_TEXTURE_1').getField('POINTS'))
+texture_points.append(supervisor.getFromDef('CUST_TEXTURE_2').getField('POINTS'))
+texture_points.append(supervisor.getFromDef('CUST_TEXTURE_3').getField('POINTS'))
 
-# [
-#               0 -0.5
-#               3 -0.5
-#               3 0.5
-#               0 0.5
-#             ]
 
 robot_translation = []
 robot_translation.append(supervisor.getFromDef('BLUE_PLAYER_1').getField('translation'))
@@ -73,10 +53,12 @@ def out_text_grean(text):
 
 current_working_directory = Path.cwd()
 
+
 def uprint(*text):
     with open(str(current_working_directory) + "\Sprint_log.txt",'a') as f:
         print(*text, file = f)
     print(*text )
+
 
 os.chdir(current_working_directory.parent/'Robofest_TEAM')
 
@@ -113,36 +95,34 @@ for coord_str in initial_coord:
     list_coord.append(coord)
 
 print("\033[1;34m" + 'start_time: ' + str(datetime.datetime.now()) + "\033[0;0m")
-pen_counter = 0
-color = 0xe0ffff
-ink_intensity = 0.8
-clean = True
-pen1_pen.write(True)
-_ = 0
+if is_pen:
+    color = 0xe0ffff
+    ink_intensity = 0.8
+    _ = 0
 while supervisor.step(time_step) != -1:
-    _ += 1
-    if _ == 1:
-        pen1_pen.setInkColor(0x006411, 1.0)
-        pen1_leadSize.setSFFloat(2.0)
-    if _ < 7:
-        pen1_translation.setSFVec3f([1.5, 0.0, 0.002])
-        continue
-    pen1_leadSize.setSFFloat(0.1)
-
-
-    distance_count += 1
-    pen_counter += 1
-    y_coordinate = []
-    #texture_scale.setSFVec2f([1/(pen_counter**0.1), 1])
-    # texture_point.setSFFloat(pen_counter)
     if is_pen:
-        # ink_intensity = 1.0  # random.uniform(0.0, 1.0)
-        #color = random.randrange(0xffffff)
-        #print(color)
+        _ += 1
+        if _ == 1:
+            pen1_pen.setInkColor(0x006411, 1.0)
+            pen1_leadSize.setSFFloat(2.0)
+        if _ < 7:
+            pen1_translation.setSFVec3f([1.5, 0.0, 0.002])
+            continue
 
+        pen1_leadSize.setSFFloat(0.1)
         pen1_pen.setInkColor(color, ink_intensity)
         pen1_translation.setSFVec3f([-0.1 + robot_translation[0].getSFVec3f()[0], 0.0, 0.002])
+
+    distance_count += 1
+    y_coordinate = []
+
     for i in range(len(robot_translation)):
+        # Отрисовка дорожки за роботом
+        robot_coord_x = robot_translation[i].getSFVec3f()[0] - 0.1
+        if 0.001 <= robot_coord_x <= 3.0 and p01_flag[i]:
+            texture_points[i].setMFVec3f(1, (robot_coord_x, center_of_rows[i]-0.5, 0.001))
+            texture_points[i].setMFVec3f(2, (robot_coord_x, center_of_rows[i]+0.5, 0.001))
+
         y_coordinate.append(robot_translation[i].getSFVec3f()[1])
         edge_1 = -0.5 + 1.05 * i
         edge_2 = 0.5 + 1.05 * i
@@ -155,6 +135,8 @@ while supervisor.step(time_step) != -1:
             robot_rotation[i].setSFRotation([1, 0, 0, 0])       # вектор напрпавления
 
         if robot_translation[i].getSFVec3f()[0] > 3.05 and p01_flag[i]:
+            texture_points[i].setMFVec3f(1, (3.0, center_of_rows[i]-0.5, 0.001))
+            texture_points[i].setMFVec3f(2, (3.0, center_of_rows[i]+0.5, 0.001))
             text = ' robot ' + str(i+1) + ' distance was finished within timesteps: ' + str(distance_count)
             out_text_grean(text)
             p01[i].terminate()
